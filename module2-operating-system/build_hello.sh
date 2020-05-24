546ace43bdacde6c40a5424bc1d747cb2e2f0c67
Hatinder Chawla <hatinderchawla@hotmail.com>
2020-05-24 16:34:08 +0000

    modify build file

--- module2-operating-system/build_hello.sh
+++ module2-operating-system/build_hello.sh
@@ -5,3 +5,6 @@ nasm -f elf64 -o hello64.o hello64.asm
 ld -s -o hello64 hello64.o
 
 gcc -o hello_c hello.c
+gcc -o exec_program exec_program.c
+gcc -o just_fork just_fork.c
+gcc -o fork_process fork_process.c
