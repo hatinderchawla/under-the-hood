d92f995054d2fc0affe90cbd96765525757b8a6a
Hatinder Chawla <hatinderchawla@hotmail.com>
2020-05-25 20:32:36 +0000

    updated

--- module2-operating-system/exec_program.c
+++ module2-operating-system/exec_program.c
@@ -2,7 +2,8 @@
 #include <sys/types.h>
 
 int main() {
-  char * argv_list[] = {"ls","-l",NULL};
-  execv("/usr/bin/ls", argv_list); 
+  char * argv_list[] = {"date", NULL, NULL};
+  execv("/usr/bin/date", argv_list); 
+  printf("Please print me");
   return 0;
 }
--- module2-operating-system/fork_process.c
+++ module2-operating-system/fork_process.c
@@ -6,18 +6,21 @@ int main() {
   int i = 0;
   pid_t pid;
 
+  printf("pid = %u, parent pid = %u\n", getpid(), getppid());
   pid = fork();
   if (pid != 0)
   {
-    printf("child pid = %u\n", pid);
-    printf("From child process\n");
-    char * argv_list[] = {"ls","-l",NULL};
-    execv("/usr/bin/ls", argv_list); 
+    printf("From parent process\n");
+    printf("pid = %u, parent pid = %u\n", getpid(), getppid());
+    char * argv_list[] = {"date", NULL, NULL};
+    execv("/usr/bin/date", argv_list); 
   }
   else
   {
     sleep(1);
-    printf("Completing parent process\n");
+    printf("From child process\n");
+    printf("pid = %u, parent pid = %u\n", getpid(), getppid());
+    printf("Done\n");
   }
   return 0;
 }
