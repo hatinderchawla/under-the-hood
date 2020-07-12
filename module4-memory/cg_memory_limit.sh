73c8701ff71b748a6a2117d39e664ef524bf145a
Hatinder Chawla <hatinderchawla@hotmail.com>
2020-07-12 19:33:15 +0000

    memory controller examples

---
+++ module4-memory/cg_memory_limit.sh
@@ -0,0 +1,24 @@
+CG_DIR=/sys/fs/cgroup
+echo "# Example - Memory Throttling"
+mkdir -p $CG_DIR/memory/cg1
+echo 10M > $CG_DIR/memory/cg1/memory.limit_in_bytes
+echo "# memory.limit_in_bytes"
+cat $CG_DIR/memory/cg1/memory.limit_in_bytes
+echo "# memory.max_usage_in_bytes"
+cat $CG_DIR/memory/cg1/memory.max_usage_in_bytes
+echo "# memory.usage_in_bytes" 
+cat $CG_DIR/memory/cg1/memory.usage_in_bytes
+
+echo $$ > $CG_DIR/memory/cg1/cgroup.procs
+echo "stress --vm 1 --vm-bytes 10M --timeout 10"
+stress --vm 1 --vm-bytes 10M --timeout 10
+
+echo $$ > /sys/fs/cgroup/memory/cgroup.procs
+
+echo "# memory.usage_in_bytes" 
+cat $CG_DIR/memory/cg1/memory.usage_in_bytes
+
+echo "# memory.max_usage_in_bytes" 
+cat $CG_DIR/memory/cg1/memory.max_usage_in_bytes
+
+rmdir $CG_DIR/memory/cg1
---
+++ module4-memory/cg_no_memory_limit.sh
@@ -0,0 +1,22 @@
+CG_DIR=/sys/fs/cgroup
+echo "# Example - No memory throttling"
+mkdir -p $CG_DIR/memory/cg1
+#echo 100000 > $CG_DIR/memory/cg1/memory.cfs_quota_us
+echo "# memory.max_usage_in_bytes"
+cat $CG_DIR/memory/cg1/memory.max_usage_in_bytes
+echo "# memory.usage_in_bytes" 
+cat $CG_DIR/memory/cg1/memory.usage_in_bytes
+
+echo $$ > $CG_DIR/memory/cg1/cgroup.procs
+echo "stress --vm 1 --vm-bytes 10M --timeout 10"
+stress --vm 1 --vm-bytes 10M --timeout 10
+
+echo $$ > /sys/fs/cgroup/memory/cgroup.procs
+
+echo "# memory.usage_in_bytes" 
+cat $CG_DIR/memory/cg1/memory.usage_in_bytes
+
+echo "# memory.max_usage_in_bytes" 
+cat $CG_DIR/memory/cg1/memory.max_usage_in_bytes
+
+rmdir $CG_DIR/memory/cg1
